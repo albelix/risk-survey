7c5efbfcd94ec674ee909ea7a613150724c2e6ad
albelix <albelix@yandex.ru>
2018-04-20 16:55:40 +0300

    UG2004_4

--- settings.py
+++ settings.py
@@ -389,7 +389,7 @@ SESSION_CONFIGS = [
         'name': 'ultimatum_simple_rus_no_reg',
         'display_name': "Ultimatum Game no Regions",
         'num_demo_participants': 2,
-        'app_sequence': ['ultimatum_simple_rus_no_reg']
+        'app_sequence': ['ultimatum_simple_rus_no_reg', 'my_survey']
     },
     {
         'name': 'trust_rus_no_reg',
